c74db753a4727ad9d8520137151308185ed15a14
zohaibm08 <zohaibmohammed30@gmail.com>
2025-07-31 21:04:35 -0400

    changed css files for navabr to look more apealing, added main content and link in nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import  Navbar  from './components/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import './App.css'
 import { Sidebar } from './components/Sidebar'
 
@@ -9,10 +8,16 @@ function App() {
 
   return (
     <>
-        <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
-        <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-        <main className="content">
-        </main>
+      <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+      <hr className="navbar-hr" />
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+      <main className="content">
+        <h2> Our Gear </h2>
+        <h3> High-Quality Equipment for Creativity </h3>
+        <h4>
+          At Gallery Studio, we pride ourselves on providing top-of-the-line equipment to enhance your music production experience. From professional microphones to state-of-the-art mixing consoles, our gear is designed to support your creative vision and bring your music to life.
+        </h4>
+      </main>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
+import GalleryStudioLogo from '../assets/GalleryStudioLogo.avif';
 
 function Navbar({ onMenuClick }) {
     return (
         <nav className="navbar">
-            <div className="navbar-container">
+            <div className="navbar-flex">
+                <Link to="/" className="navbar-logo">
+                    <img src={GalleryStudioLogo} alt="Gallery Studio Logo" />
+                </Link>
                 <h1 className="navbar-title">Gallery Studio</h1>
                 <button className="menu-btn" onClick={onMenuClick} aria-label="Open sidebar">
                     &#9776;
